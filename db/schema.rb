fa0f324bcf0155da75c2f04fd5ce764ca45a2a3e
Diego Michel <diegomichel@gmail.com>
2022-06-15 21:13:18 -0600

    Add user to task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_16_030153) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_16_030256) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_16_030153) do
     t.integer "completed_count", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "tasks_tags", id: false, force: :cascade do |t|
@@ -54,4 +56,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_16_030153) do
   end
 
   add_foreign_key "tags", "users"
+  add_foreign_key "tasks", "users"
 end
