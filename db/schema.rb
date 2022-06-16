0559ff84c1197a92309dc0e87217cf89f0598edb
Diego Michel <diegomichel@gmail.com>
2022-06-15 21:02:45 -0600

    add user to tag migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_16_024611) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_16_030153) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_16_024611) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_tags_on_user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
@@ -51,4 +53,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_16_024611) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tags", "users"
 end
